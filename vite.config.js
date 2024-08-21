5c909a71c031ebe30d459becf829eb103996738e
Yancey <1709185482@qq.com>
2024-08-21 21:08:33 +0800

    remove obfuscator

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 import importToCDN from 'vite-plugin-cdn-import';
-import obfuscator from 'rollup-plugin-obfuscator';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -28,64 +27,6 @@ export default defineConfig({
                 },
             ],
         }),
-        obfuscator({
-            global:false,
-            options: {
-                compact: true,
-                controlFlowFlattening: false,
-                controlFlowFlatteningThreshold: 0.75,
-                deadCodeInjection: false,
-                deadCodeInjectionThreshold: 0.4,
-                debugProtection: false,
-                debugProtectionInterval: 0,
-                disableConsoleOutput: false,
-                domainLock: [],
-                domainLockRedirectUrl: 'about:blank',
-                forceTransformStrings: [],
-                identifierNamesCache: null,
-                identifierNamesGenerator: 'hexadecimal',
-                identifiersDictionary: [],
-                identifiersPrefix: '',
-                ignoreImports: false,
-                inputFileName: '',
-                log: false,
-                numbersToExpressions: false,
-                optionsPreset: 'default',
-                renameGlobals: false,
-                renameProperties: false,
-                renamePropertiesMode: 'safe',
-                reservedNames: [],
-                reservedStrings: [],
-                seed: 0,
-                selfDefending: false,
-                simplify: true,
-                sourceMap: false,
-                sourceMapBaseUrl: '',
-                sourceMapFileName: '',
-                sourceMapMode: 'separate',
-                sourceMapSourcesMode: 'sources-content',
-                splitStrings: false,
-                splitStringsChunkLength: 10,
-                stringArray: true,
-                stringArrayCallsTransform: true,
-                stringArrayCallsTransformThreshold: 0.5,
-                stringArrayEncoding: [],
-                stringArrayIndexesType: [
-                    'hexadecimal-number'
-                ],
-                stringArrayIndexShift: true,
-                stringArrayRotate: true,
-                stringArrayShuffle: true,
-                stringArrayWrappersCount: 1,
-                stringArrayWrappersChainedCalls: true,
-                stringArrayWrappersParametersMaxCount: 2,
-                stringArrayWrappersType: 'variable',
-                stringArrayThreshold: 0.75,
-                target: 'browser',
-                transformObjectKeys: false,
-                unicodeEscapeSequence: false
-            }
-        })
     ],
     esbuild: {
         drop: ['console', 'debugger'],
