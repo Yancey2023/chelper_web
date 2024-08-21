1a8c502056c11768969a08c0a634168e9c93ab38
Yancey <1709185482@qq.com>
2024-08-21 19:29:25 +0800

    remove log

--- src/App.vue
+++ src/App.vue
@@ -50,7 +50,6 @@ export default {
       this.loadMore(Math.floor(this.$refs.listRef.clientHeight / 25))
     },
     loadMore(count) {
-      console.log("try load more: " + count)
       const start = this.suggestions.length
       const end = Math.min(start + count, this.realSuggestionSize)
       for (let i = start; i < end; i++) {
