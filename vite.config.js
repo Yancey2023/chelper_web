6628e025d9afc68fa223219dda9903dbc249b136
Yancey <1709185482@qq.com>
2024-08-21 20:44:31 +0800

    try use obfuscator

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,99 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 import importToCDN from 'vite-plugin-cdn-import';
+import obfuscator from 'rollup-plugin-obfuscator';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-    importToCDN({
-      modules: [
-        {
-          name: 'vue',
-          var: 'Vue',
-          // path: 'https://cdn.bootcdn.net/ajax/libs/vue/3.4.31/vue.runtime.global.prod.min.js',
-          path: 'https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/vue/3.2.31/vue.global.prod.min.js',
-        },
-      ],
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  base: '/chelper_web/'
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+        }),
+        importToCDN({
+            modules: [
+                {
+                    name: 'vue',
+                    var: 'Vue',
+                    // path: 'https://cdn.bootcdn.net/ajax/libs/vue/3.4.31/vue.runtime.global.prod.min.js',
+                    path: 'https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/vue/3.2.31/vue.global.prod.min.js',
+                },
+            ],
+        }),
+        obfuscator({
+            global:false,
+            options: {
+                compact: true,
+                controlFlowFlattening: false,
+                controlFlowFlatteningThreshold: 0.75,
+                deadCodeInjection: false,
+                deadCodeInjectionThreshold: 0.4,
+                debugProtection: false,
+                debugProtectionInterval: 0,
+                disableConsoleOutput: false,
+                domainLock: [],
+                domainLockRedirectUrl: 'about:blank',
+                forceTransformStrings: [],
+                identifierNamesCache: null,
+                identifierNamesGenerator: 'hexadecimal',
+                identifiersDictionary: [],
+                identifiersPrefix: '',
+                ignoreImports: false,
+                inputFileName: '',
+                log: false,
+                numbersToExpressions: false,
+                optionsPreset: 'default',
+                renameGlobals: false,
+                renameProperties: false,
+                renamePropertiesMode: 'safe',
+                reservedNames: [],
+                reservedStrings: [],
+                seed: 0,
+                selfDefending: false,
+                simplify: true,
+                sourceMap: false,
+                sourceMapBaseUrl: '',
+                sourceMapFileName: '',
+                sourceMapMode: 'separate',
+                sourceMapSourcesMode: 'sources-content',
+                splitStrings: false,
+                splitStringsChunkLength: 10,
+                stringArray: true,
+                stringArrayCallsTransform: true,
+                stringArrayCallsTransformThreshold: 0.5,
+                stringArrayEncoding: [],
+                stringArrayIndexesType: [
+                    'hexadecimal-number'
+                ],
+                stringArrayIndexShift: true,
+                stringArrayRotate: true,
+                stringArrayShuffle: true,
+                stringArrayWrappersCount: 1,
+                stringArrayWrappersChainedCalls: true,
+                stringArrayWrappersParametersMaxCount: 2,
+                stringArrayWrappersType: 'variable',
+                stringArrayThreshold: 0.75,
+                target: 'browser',
+                transformObjectKeys: false,
+                unicodeEscapeSequence: false
+            }
+        })
+    ],
+    esbuild: {
+        drop: ['console', 'debugger'],
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    base: '/chelper_web/'
 })
