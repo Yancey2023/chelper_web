d2851e236df56161bf1c6d61bdbaa399b19385b2
Yancey <1709185482@qq.com>
2025-04-19 16:33:27 +0800

    remove unnecessary dependencies

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,12 @@ import {fileURLToPath, URL} from 'node:url'
 
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
-import importToCDN from 'vite-plugin-cdn-import';
+import importToCDN from 'vite-plugin-cdn-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
-        AutoImport({
-            resolvers: [ElementPlusResolver()],
-        }),
-        Components({
-            resolvers: [ElementPlusResolver()],
-        }),
         importToCDN({
             modules: [
                 {
@@ -28,6 +19,9 @@ export default defineConfig({
             ],
         }),
     ],
+    build: {
+        minify: "esbuild",
+    },
     esbuild: {
         drop: ['debugger'],
     },
