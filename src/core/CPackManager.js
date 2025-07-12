a30f0bde2b483288bc99f92de1769cfe82751cc5
Yancey <1709185482@qq.com>
2025-07-13 00:41:19 +0800

    update CHelper-Core

--- src/core/CPackManager.js
+++ src/core/CPackManager.js
@@ -1,60 +1,60 @@
-import {CHelperCore, createWasmFuture} from '@/core/libCHelperWeb.js';
+import { CHelperCore, createWasmFuture } from '@/core/libCHelperWeb.js'
 
-const VERSION_RELEASE = '1.21.92.1';
-const VERSION_BETA = '1.21.100.23';
-const VERSION_NETEASE = '1.21.0.03';
+const VERSION_RELEASE = '1.21.92.1'
+const VERSION_BETA = '1.21.100.23'
+const VERSION_NETEASE = '1.21.0.03'
 
-export const DEFAULT_BRANCH = 'release-experiment';
+export const DEFAULT_BRANCH = 'release-experiment'
 
 export const ALL_BRANCH = [
-    "release-vanilla",
-    "release-experiment",
-    "beta-vanilla",
-    "beta-experiment",
-    "netease-vanilla",
-    "netease-experiment"
-];
+  'release-vanilla',
+  'release-experiment',
+  'beta-vanilla',
+  'beta-experiment',
+  'netease-vanilla',
+  'netease-experiment',
+]
 
 export const ALL_BRANCH_CHINESE = [
-    "正式版-原版-" + VERSION_RELEASE,
-    "正式版-实验性玩法-" + VERSION_RELEASE,
-    "测试版-原版-" + VERSION_BETA,
-    "测试版-实验性玩法-" + VERSION_BETA,
-    "中国版-原版-" + VERSION_NETEASE,
-    "中国版-实验性玩法-" + VERSION_NETEASE
-];
+  '正式版-原版-' + VERSION_RELEASE,
+  '正式版-实验性玩法-' + VERSION_RELEASE,
+  '测试版-原版-' + VERSION_BETA,
+  '测试版-实验性玩法-' + VERSION_BETA,
+  '中国版-原版-' + VERSION_NETEASE,
+  '中国版-实验性玩法-' + VERSION_NETEASE,
+]
 
-let cpackCache = {};
+let cpackCache = {}
 
 export async function getCore(branch) {
-    let cpack = cpackCache[branch]
-    if (cpack === undefined) {
-        cpack = await fetch(getRealFileName(branch))
-            .then((response) => response.arrayBuffer())
-            .then(async (cpack) => {
-                return new Uint8Array(cpack);
-            });
-        cpackCache[branch] = cpack;
-    }
-    await createWasmFuture;
-    return new CHelperCore(cpack);
+  let cpack = cpackCache[branch]
+  if (cpack === undefined) {
+    cpack = await fetch(getRealFileName(branch))
+      .then((response) => response.arrayBuffer())
+      .then(async (cpack) => {
+        return new Uint8Array(cpack)
+      })
+    cpackCache[branch] = cpack
+  }
+  await createWasmFuture
+  return new CHelperCore(cpack)
 }
 
 export function getRealFileName(branch) {
-    switch (branch) {
-        case "release-vanilla" :
-            return "release-vanilla-" + VERSION_RELEASE + ".cpack";
-        case "release-experiment":
-            return "release-experiment-" + VERSION_RELEASE + ".cpack";
-        case "beta-vanilla":
-            return "beta-vanilla-" + VERSION_BETA + ".cpack";
-        case "beta-experiment" :
-            return "beta-experiment-" + VERSION_BETA + ".cpack";
-        case "netease-vanilla":
-            return "netease-vanilla-" + VERSION_NETEASE + ".cpack";
-        case "netease-experiment" :
-            return "netease-experiment-" + VERSION_NETEASE + ".cpack";
-        default :
-            return getRealFileName(DEFAULT_BRANCH);
-    }
+  switch (branch) {
+    case 'release-vanilla':
+      return 'release-vanilla-' + VERSION_RELEASE + '.cpack'
+    case 'release-experiment':
+      return 'release-experiment-' + VERSION_RELEASE + '.cpack'
+    case 'beta-vanilla':
+      return 'beta-vanilla-' + VERSION_BETA + '.cpack'
+    case 'beta-experiment':
+      return 'beta-experiment-' + VERSION_BETA + '.cpack'
+    case 'netease-vanilla':
+      return 'netease-vanilla-' + VERSION_NETEASE + '.cpack'
+    case 'netease-experiment':
+      return 'netease-experiment-' + VERSION_NETEASE + '.cpack'
+    default:
+      return getRealFileName(DEFAULT_BRANCH)
+  }
 }
