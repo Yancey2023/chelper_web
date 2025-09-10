c0ec3c464f39e68de390801337b7029dbb9a7606
Yancey <1709185482@qq.com>
2025-09-10 06:06:42 -0400

    reformat files

--- README.md
+++ README.md
@@ -11,17 +11,17 @@ It is writing by c++. Please refer to [CHelper-Core](https://github.com/Yancey20
 ## Project Setup
 
 ```sh
-npm install
+pnpm install
 ```
 
 ### Compile and Hot-Reload for Development
 
 ```sh
-npm run dev
+pnpm dev
 ```
 
 ### Compile and Minify for Production
 
 ```sh
-npm run build
+pnpm build
 ```
--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,20 @@
-import {fileURLToPath, URL} from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-    ],
-    build: {
-        minify: "esbuild",
+  plugins: [vue()],
+  build: {
+    minify: 'esbuild',
+  },
+  esbuild: {
+    drop: ['debugger'],
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-    esbuild: {
-        drop: ['debugger'],
-    },
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    }
+  },
 })
